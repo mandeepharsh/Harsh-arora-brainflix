aeae4cc341cc069820032078552b0a15112de89f
mandeepharsh <harshman47@gmail.com>
2023-05-20 02:21:36 -0400

    layout complete for input commponet

---
+++ src/components/CommentInput/CommentInput.jsx
@@ -0,0 +1,21 @@
+import userImage from "../../Assets/Images/Mohan-muruge.jpg";
+
+
+import "./CommentInput.scss"
+
+const CommentInput = () => {
+  return (
+    <div className="comment-input">
+        <img className ="comment-input__profile-img" alt ="proflie " src={userImage}/>
+         {/* <form className="comment-input__content"> */}
+        <div className="comment-input__text-area">
+            <p className="comment-input__label">join the description</p>
+            <textarea className="comment-input__field" placeholder="Add a new comment"/>
+        </div>
+        <button className="comment-input__button" >Comment</button>
+        {/* </form> */}
+    </div>
+  )
+}
+
+export default CommentInput
--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,14 +1,18 @@
+// Importing componets being used in the main seciton
 import CommentCard from "../CommentCard/CommentCard"
-import "./Comments.scss";
-import img from "../../Assets/Images/Mohan-muruge.jpg"
+import CommentInput from "../CommentInput/CommentInput";
 import VideoDetails from "../VideoDetails/VideoDetails";
 
+
+// Scss file
+import "./Comments.scss";
 
 const Comments = ({selectedVideo}) => {
   return (
     <div className="reviews">
     <VideoDetails selectedVideo = {selectedVideo} />
     <span className="reviews__count">{selectedVideo.comments.length } Comments</span>
+    <CommentInput/>
       <div className="reviews__comments">
       {selectedVideo.comments.map((video) =>{
        return(  <CommentCard key={video.id}
