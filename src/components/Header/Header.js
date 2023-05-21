0309bdfccfd436b4e8f2dd65c3cabd46eb6bc895
mandeepharsh <harshman47@gmail.com>
2023-05-20 20:14:44 -0400

    fix height and padding of searchbar and buttons.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,19 @@
 import logo from "../../Assets/Logo/BrainFlix-logo.svg";
 import img from "../../Assets/Images/Mohan-muruge.jpg"
 
+
 import './Header.scss';
 const Header = () => {
+
+
   return (
     <nav className = "nav-bar">
       <img className = "nav-bar__logo" src ={logo} alt = "Logo"/>
-      <form className = "nav-bar__form-field">
-      <input placeholder="Search" className = "nav-bar__search-bar"/>
+      <form className = "nav-bar__form-field "  >
+      <input placeholder="Search" name="search" className = "nav-bar__search-bar" />
       <img className ="nav-bar__profile-img" alt ="proflie " src={img}/>
       <button className="nav-bar__button">
-        {"upload".toUpperCase()}
+        {"upload"}
     </button>
       </form>
     </nav>
